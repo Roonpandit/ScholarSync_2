342acc6a91743f834c12974d3ad1bec8832c2586
Tarun <tarunvashisth0000@gmail.com>
2025-05-16 00:36:44 +0530

    backend completed

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // Enable CORS with specific origin and credentials
 const corsOptions = {
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: process.env.ALLOWED_ORIGINS.split(","), // Your frontend URL
   credentials: true, // Allow credentials (cookies, authorization headers)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
