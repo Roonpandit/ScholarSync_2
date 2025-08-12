6a31d6160801fe153ff58de5e5da8b9b3d2e3201
Tarun <tarunvashisth0000@gmail.com>
2025-08-12 22:42:45 +0530

    Forget Password implemented

--- Backend/services/emailService.js
+++ Backend/services/emailService.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: process.env.EMAIL_SECURE,
-  requireTLS: process.env.EMAIL_REQUIRETLS,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
